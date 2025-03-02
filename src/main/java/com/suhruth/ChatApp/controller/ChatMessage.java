7b5d6aaf4676e11f4e39cc02344a68b2155a3c70
Suhruth <suhruthyambakam822001@gmail.com>
2025-03-02 12:47:29 +0530

    first commit

---
+++ src/main/java/com/suhruth/ChatApp/controller/ChatMessage.java
@@ -0,0 +1,42 @@
+package com.suhruth.ChatApp.controller;
+
+public class ChatMessage {
+
+	private int id;
+	private String sender;
+	private String content;
+	
+	public ChatMessage() {}
+
+	public ChatMessage(int id, String sender, String content) {
+		super();
+		this.id = id;
+		this.sender = sender;
+		this.content = content;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSender() {
+		return sender;
+	}
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
